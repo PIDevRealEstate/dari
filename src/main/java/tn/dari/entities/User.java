a1ad88b279caf73b0e1dea72be8ae712ab4d99bb
khaledboussaidi <khaled.boussaidi@etudiant-enit.utm.tn>
2021-05-09 03:35:38 +0200

    updated by khaled boussaidi

--- src/main/java/tn/dari/entities/User.java
+++ src/main/java/tn/dari/entities/User.java
@@ -47,7 +47,6 @@ public class User {
 	
 	public User(int id, String firstName, String lastName, String email, String password,List<Furniture> favoris) {
 		super();
-		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
@@ -58,13 +57,7 @@ public class User {
 		
 	}
 
-	public int getId() {
-		return id;
-	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getFirstName() {
 		return firstName;
